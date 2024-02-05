8ee97b7671fa30a0f3f78e57bf02019a320a1ace
Michel Casabianca <mcasabianca@sfbx.io>
2024-02-05 17:10:02 +0100

    Implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::vec;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in std::env::args().skip(1) {
+        let i: u64 = arg.trim().parse().expect("Not a number!");
+        syracuse(i);
+    }
+}
+
+fn syracuse(mut n: u64) {
+    let mut max: u64 = n;
+    let mut values: Vec<u64> = vec![n];
+    while n != 1 {
+        if n % 2 == 0 {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        if n > max {
+            max = n;
+        }
+        values.push(n);
+    }
+    println!("{:?}, max: {max}, count: {}", values, values.len());
 }
